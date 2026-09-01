47f776176840919b86029748cbf62780622d419b
Almira Vasiluk <bezzbashennaya@gmail.com>
2026-09-01 10:51:20 +0300

    created TransactionPage

--- src/widgets/transaction-card/TransactionCard.tsx
+++ src/widgets/transaction-card/TransactionCard.tsx
@@ -1,6 +1,26 @@
-import { Box } from "@mui/material"
+import { Box } from "@mui/material";
+import type React from "react";
 
-const TransactionCard = () => {
+interface Transaction {
+  icon: string;
+  name: string;
+  category: string;
+  date: string;
+  locate: string;
+  country: string;
+  transactionId: string;
+  price: string;
+  status: string;
+  commission: string;
+  transactionCode: string;
+}
+
+interface TransactionCardProps {
+  transaction: Transaction;
+  onClick?: () => void;
+}
+
+const TransactionCard :React.FC<TransactionCardProps> = () => {
     return (
         <Box>
             <Box>
